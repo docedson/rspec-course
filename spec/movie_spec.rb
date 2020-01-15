28546d3b53cee961d1023481379baea954861e93
ae074267 <C_Edson.Andrew@cerner.com>
2020-01-15 16:49:55 -0600

    Finished Part 53 - The allow Method

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -53,6 +53,7 @@ RSpec.describe Movie do
       # expect(stuntman).to receive(:light_on_fire).once
       # expect(stuntman).to receive(:light_on_fire).exactly(1).times
       expect(stuntman).to receive(:light_on_fire).at_most(1).times
+      
       # expect(stuntman).to receive(:act).twice
       # expect(stuntman).to receive(:act).exactly(2).times
       expect(stuntman).to receive(:act).at_least(2).times
